8a864998e719fa0a13608a925988607d5a03e871
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-08-27 16:26:19 +0200

    // Version updated to v0.7.0

--- dashgoals.php
+++ dashgoals.php
@@ -40,7 +40,7 @@ class DashGoals extends Module
 	{
 		$this->name = 'dashgoals';
 		$this->tab = 'dashboard';
-		$this->version = '0.6.6';
+		$this->version = '0.7.0';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
